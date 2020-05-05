65b32c5fd31024e862405e5c7196c5e2d3e87d40
Yussef <saidi.yussef@live.fr>
2020-05-05 17:55:01 -0400

    Add Auth Subcomponent

--- app/src/main/java/com/yussefsaidi/daggerproject/di/ActivityBuildersModule.java
+++ app/src/main/java/com/yussefsaidi/daggerproject/di/ActivityBuildersModule.java
@@ -12,7 +12,6 @@ public abstract class ActivityBuildersModule {
     @ContributesAndroidInjector(
             modules = {
                     AuthViewModelsModule.class
-            }
-    )
+            })
     abstract AuthActivity contributeAuthActivity();
 }
