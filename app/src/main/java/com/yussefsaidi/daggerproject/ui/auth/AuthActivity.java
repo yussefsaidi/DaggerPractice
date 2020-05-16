af1d1e6a475c6c246af501892f464c3454179fa6
Yussef <saidi.yussef@live.fr>
2020-05-16 16:49:41 -0400

    Setup authentication

--- app/src/main/java/com/yussefsaidi/daggerproject/ui/auth/AuthActivity.java
+++ app/src/main/java/com/yussefsaidi/daggerproject/ui/auth/AuthActivity.java
@@ -11,6 +11,8 @@ import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.bumptech.glide.RequestManager;
 import com.yussefsaidi.daggerproject.R;
@@ -28,6 +30,7 @@ public class AuthActivity extends DaggerAppCompatActivity implements View.OnClic
     private AuthViewModel viewModel;
 
     private EditText userId;
+    private ProgressBar progressBar;
 
     @Inject
     ViewModelProviderFactory providerFactory;
@@ -43,10 +46,52 @@ public class AuthActivity extends DaggerAppCompatActivity implements View.OnClic
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auth);
         userId = findViewById(R.id.user_id_input);
+        progressBar = findViewById(R.id.progress_bar);
         findViewById(R.id.login_button).setOnClickListener(this);
 
         viewModel = new ViewModelProvider(this, providerFactory).get(AuthViewModel.class);
         setLogo();
+
+        subscribeObservers();
+    }
+
+    private void subscribeObservers(){
+        viewModel.observeUser().observe(this, new Observer<AuthResource<User>>() {
+            @Override
+            public void onChanged(AuthResource<User> userAuthResource) {
+                if(userAuthResource != null){
+                    switch(userAuthResource.status){
+                        case LOADING:{
+                            showProgressBar(true);
+                            break;
+                        }
+                        case AUTHENTICATED:{
+                            showProgressBar(false);
+                            Log.d(TAG, "onChanged: LOGIN SUCCESS: " + userAuthResource.data.getEmail());
+                            break;
+                        }
+                        case NOT_AUTHENTICATED:{
+                            showProgressBar(false);
+                            break;
+                        }
+                        case ERROR:{
+                            showProgressBar(false);
+                            Toast.makeText(AuthActivity.this, userAuthResource.message + "\nDid you enter a number between 1 and 10?", Toast.LENGTH_SHORT).show();
+                            break;
+                        }
+                    }
+                }
+            }
+        });
+    }
+
+    private void showProgressBar(boolean isVisible){
+        if(isVisible){
+            progressBar.setVisibility(View.VISIBLE);
+        }
+        else {
+            progressBar.setVisibility(View.GONE);
+        }
     }
 
     private void setLogo(){
@@ -70,14 +115,7 @@ public class AuthActivity extends DaggerAppCompatActivity implements View.OnClic
         if(TextUtils.isEmpty(userId.getText().toString())){
             return;
         }
-        viewModel.authenticateWithId(Integer.parseInt(userId.getText().toString()))
-                .observe(this, new Observer<User>() {
-                    @Override
-                    public void onChanged(User user) {
-                        if(user != null){
-                            Log.d(TAG, "onChanged: " + user.getEmail());
-                        }
-                    }
-                });
+        viewModel.authenticateWithId(Integer.parseInt(userId.getText().toString()));
+
     }
 }
--- app/src/main/java/com/yussefsaidi/daggerproject/ui/auth/AuthViewModel.java
+++ app/src/main/java/com/yussefsaidi/daggerproject/ui/auth/AuthViewModel.java
@@ -1,21 +1,15 @@
 package com.yussefsaidi.daggerproject.ui.auth;
 
-import android.util.Log;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.LiveDataReactiveStreams;
 import androidx.lifecycle.MediatorLiveData;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModel;
-
 import com.yussefsaidi.daggerproject.models.User;
 import com.yussefsaidi.daggerproject.network.auth.AuthApi;
-
 import javax.inject.Inject;
-
-import io.reactivex.rxjava3.annotations.NonNull;
-import io.reactivex.rxjava3.core.Observer;
-import io.reactivex.rxjava3.core.Scheduler;
-import io.reactivex.rxjava3.disposables.Disposable;
+import io.reactivex.rxjava3.functions.Function;
 import io.reactivex.rxjava3.schedulers.Schedulers;
 
 public class AuthViewModel extends ViewModel {
@@ -23,42 +17,53 @@ public class AuthViewModel extends ViewModel {
     private static final String TAG = "AuthViewModel";
 
     private final AuthApi authApi;
+
+    private MediatorLiveData<AuthResource<User>> authUser = new MediatorLiveData<>();
     
     @Inject
     public AuthViewModel(AuthApi authApi) {
         this.authApi = authApi;
-
-        authApi.getUser(1)
-                .toObservable()
-                .subscribeOn(Schedulers.io())
-                .subscribe(new Observer<User>() {
-                    @Override
-                    public void onSubscribe(@NonNull Disposable d) {
-
-                    }
-
-                    @Override
-                    public void onNext(@NonNull User user) {
-                        Log.d(TAG, "onNext: " + user.getEmail());
-                    }
-
-                    @Override
-                    public void onError(@NonNull Throwable e) {
-                        Log.e(TAG, "onError: ", e);
-                    }
-
-                    @Override
-                    public void onComplete() {
-
-                    }
-                });
     }
 
-    public LiveData<User> authenticateWithId(int userId){
-        final LiveData<User> source = LiveDataReactiveStreams.fromPublisher(
+    public void authenticateWithId(int userId){
+        authUser.setValue(AuthResource.loading(null));
+
+        final LiveData<AuthResource<User>> source = LiveDataReactiveStreams.fromPublisher(
                 authApi.getUser(userId)
+                        .onErrorReturn(new Function<Throwable, User>() {
+                            @Override
+                            public User apply(Throwable throwable) throws Throwable {
+                                User errorUser = new User();
+                                errorUser.setId(-1);
+                                return errorUser;
+                            }
+                        })
+
+
+                        .map(new Function<User, AuthResource<User>>() {
+                            @Override
+                            public AuthResource<User> apply(User user) throws Throwable {
+
+                                if(user.getId() == -1){
+                                    return AuthResource.error("Could not authenticate", null);
+                                }
+                                        return AuthResource.authenticated(user);
+                            }
+                        })
+
                 .subscribeOn(Schedulers.io())
         );
-        return source;
+
+        authUser.addSource(source, new Observer<AuthResource<User>>() {
+            @Override
+            public void onChanged(AuthResource<User> user) {
+                authUser.setValue(user);
+                authUser.removeSource(source);
+            }
+        });
+    }
+
+    public LiveData<AuthResource<User>> observeUser(){
+        return authUser;
     }
 }
