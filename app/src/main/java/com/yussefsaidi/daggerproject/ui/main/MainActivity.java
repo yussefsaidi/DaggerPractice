37719b85b94cb548dcdebea6b8e9953b67927575
Yussef <saidi.yussef@live.fr>
2020-05-17 15:58:21 -0400

    Setup logout menu in Main

--- app/src/main/java/com/yussefsaidi/daggerproject/ui/main/MainActivity.java
+++ app/src/main/java/com/yussefsaidi/daggerproject/ui/main/MainActivity.java
@@ -1,12 +1,16 @@
 package com.yussefsaidi.daggerproject.ui.main;
 
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.Toast;
-
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-
 import com.yussefsaidi.daggerproject.BaseActivity;
 import com.yussefsaidi.daggerproject.R;
+import com.yussefsaidi.daggerproject.ui.main.profile.ProfileFragment;
 
 public class MainActivity extends BaseActivity {
 
@@ -17,7 +21,25 @@ public class MainActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(this, "MainActivity", Toast.LENGTH_SHORT).show();
+
+
+    }
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
     }
 
-
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.logout:{
+                sessionManager.logOut();
+                return true;
+            }
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
